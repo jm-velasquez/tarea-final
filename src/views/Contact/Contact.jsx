0c643c13c7ed3a848470836695a622b049537045
jm-velasquez <115376537+jm-velasquez@users.noreply.github.com>
2022-12-11 02:41:35 -0300

    Se agrega formulario de contacto Link de Contacto

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import Form from 'react-bootstrap/Form';
 
 export default function Contact() {
-  return <div>ESTO ES UNA PAGINA DE CONTACTO</div>;
+  return (
+
+<Form>
+      
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Dirección de Correo Electrónico</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Mensaje de contacto</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+    </Form>
+
+  );
 }
